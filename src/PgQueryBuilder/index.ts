2f210f2345ac45c2e46d463df1ed2dcdcccefebb
kuljitsingh-crypto <kuljits093@gmail.com>
2025-08-30 13:23:35 +0530

    updated

--- src/PgQueryBuilder/index.ts
+++ src/PgQueryBuilder/index.ts
@@ -2,6 +2,6 @@ export * from './globalTypes';
 export * from './constants/dataTypes';
 export { aggregateFn as aggrFn } from './methods/aggregateFunctionHelper';
 export { fieldFn } from './methods/fieldFunctionHelper';
-export { typeCastFn } from './methods/typeCastHelper';
+export { typeCastFn as castFn } from './methods/typeCastHelper';
 export { utilFn } from './methods/globalFunctionHelper';
 export { DBModel } from './model.helpers';
